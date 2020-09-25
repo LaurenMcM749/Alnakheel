1bf56ea15af95e9ef14efaabd710c79328223be9
Lauren McMillen <laurenamcmillen@gmail.com>
2020-09-25 21:03:24 +0400

    changed url to be at bottom of list

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,13 @@ from django.urls import path
 
 
 urlpatterns = [
-    path(r'', RedirectView.as_view(url='home', permanent=False)),
-    path('home', views.PostList.as_view(), name='home'),
-    path('home/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('home/arts', views.ArtsPostList.as_view(), name='arts'),
-    path('home/books', views.BooksPostList.as_view(), name='books'),
-    path('home/films', views.FilmsPostList.as_view(), name='films'),
-    path('home/travel', views.TravelPostList.as_view(), name='travel'),
-    path('home//interviews/', views.InterviewsPostList.as_view(), name='interviews'),
-    path('home/about', views.AboutView.as_view(), name='about'),
+    # path(r'', RedirectView.as_view(url='home', permanent=False)),
+    path('arts/', views.ArtsPostList.as_view(), name='arts'),
+    path('books/', views.BooksPostList.as_view(), name='books'),
+    path('films/', views.FilmsPostList.as_view(), name='films'),
+    path('travel/', views.TravelPostList.as_view(), name='travel'),
+    path('interviews/', views.InterviewsPostList.as_view(), name='interviews'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', views.PostList.as_view(), name='home'),
 ]
